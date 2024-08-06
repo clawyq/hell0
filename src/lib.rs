60f558e0d6b0fdb57f86b5251649f553c4917b78
clawyq <clintonlaw96@gmail.com>
2024-08-06 16:18:41 +0800

    thank u rust book

---
+++ src/lib.rs
@@ -0,0 +1,98 @@
+use std::fmt;
+use std::{
+    sync::{Arc, Mutex, mpsc},
+    thread
+};
+
+#[derive(Debug)]
+pub struct PoolCreationError;
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Pool cannot be created with no threads")
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: u32,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    /// Creates a new Worker
+    /// 
+    /// # Arguments
+    /// 
+    /// * `id` - unique identifier for the worker
+    /// * `receiver` - shared receiver for receiving jobs
+    fn new(id: u32, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || loop {
+            let payload = receiver.lock().unwrap().recv();
+            match payload {
+                Ok(job) => {
+                    println!("Worker {id} is on it!!");
+                    job();
+                }
+                Err(msg) => {
+                    println!("Worker {id} disconnecting: {msg}");
+                    break;
+                }
+            }
+        });
+        Worker { id, thread: Some(thread) }
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>
+}
+
+impl ThreadPool {
+    /// Creates a new ThreadPool with the specified capacity.
+    ///
+    /// # Arguments
+    ///
+    /// * `capacity` - The number of worker threads to create.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(ThreadPool)` if the pool was successfully created.
+    /// * `Err(PoolCreationError)` if the capacity is zero.
+    pub fn build(capacity: u32) -> Result<ThreadPool, PoolCreationError> {
+        if capacity == 0 {
+            return Err(PoolCreationError)
+        }
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let workers = (0..capacity)
+            .map(|id| Worker::new(id, Arc::clone(&receiver)))
+            .collect();
+        Ok(ThreadPool { workers, sender: Some(sender) })
+    }
+
+    /// Executes a job on the thread pool.
+    ///
+    /// # Arguments
+    ///
+    /// * `f` - The job to be executed.
+    pub fn execute<F>(&self, f: F)
+    where 
+        F: FnOnce() + Send + 'static
+    {
+        self.sender.as_ref().unwrap().send(Box::new(f)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.workers {
+            println!("byebye -love, Worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+use hell0::ThreadPool;
+
+fn main() {
+    const MAX_WORKERS: u32 = 5;
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::build(MAX_WORKERS).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request: String = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, source_html_file_name) = match &request[..]  {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "err.html")
+    };
+    let contents: String = fs::read_to_string(source_html_file_name).unwrap();
+    let content_length: usize = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{contents}");
+    
+    stream.write_all(response.as_bytes()).unwrap();
+}
